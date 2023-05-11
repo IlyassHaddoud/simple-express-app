7ce06970738f17a17dfa5cddc2c4c44124f06ad3
ilyass <79806692+illyasshaddoud@users.noreply.github.com>
2023-05-11 16:04:48 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const PORT = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world from zed</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log("App running on " + PORT);
+});
